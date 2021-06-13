e9b45beb41f05d7b6a66367f37572c6a172e6cd2
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-06-13 19:31:56 +0530

    Player rolls the die

--- src/snake_ladder.java
+++ src/snake_ladder.java
@@ -1,6 +1,8 @@
 public class snake_ladder {
     public static void main(String args[]){
         int player1Position = 0;
+        int dieVal = (int)Math.floor(Math.random()*10)%6 +1;
+        System.out.println(dieVal);
 
     }
 }
