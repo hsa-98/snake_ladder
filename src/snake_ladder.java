a6c4b8e6f25a4684ee9bde3901ea669e2e8319c4
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-06-13 20:16:01 +0530

    Tells the number of times the die was rolles

--- src/snake_ladder.java
+++ src/snake_ladder.java
@@ -4,17 +4,18 @@ public class snake_ladder {
     public static final int Ladder = 1;
     public static final int Snake = 2;
     public static void main(String args[]){
-        int player1Position, dieVal, playerOption; //variables
+        int player1Position, dieVal, playerOption,dieRollNo; //variables
         player1Position = 0;
+        dieRollNo = 0;
         while(player1Position < 100){
-            dieVal = (int) Math.floor(Math.random() * 10) % 6 + 1; //calculates die value
             playerOption = (int) Math.floor(Math.random() * 10) % 3; //calculates die option
             switch (playerOption) {
                 case No_Move:
                     System.out.println("Player doesnt move");
                     break;
                 case Ladder:
-
+                    dieRollNo++;
+                    dieVal = (int) Math.floor(Math.random() * 10) % 6 + 1; //calculates die value
 
                     if(player1Position +dieVal>100){
                         System.out.println("The player doesnt move becasue he will exceed 100");
@@ -27,6 +28,8 @@ public class snake_ladder {
                     System.out.println("The player is at " + player1Position);
                     break;
                 case Snake:
+                    dieRollNo++;
+                    dieVal = (int) Math.floor(Math.random() * 10) % 6 + 1; //calculates die value
                     System.out.println("Player moves back by " + dieVal + " positions");
                     player1Position -= dieVal;
                     if (player1Position < 0) {
@@ -36,5 +39,6 @@ public class snake_ladder {
                     break;
             }
         }
+        System.out.println("The no. of times the die rolled was "+dieRollNo);
     }
 }
